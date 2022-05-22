b168d77080986ae57ee2ad783a046afcfb1ddecb
guadalupe182 <guadalupe.rhinojosa@alumnos.udg.mx>
2022-05-22 17:33:27 -0500

    agregado de dependecias

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Web Address Book</title>
+    <link rel="icon" href="public/img/logos/favicon.png" type="image/png">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once "dependencies.php"; ?>
+</head>
+<body>
+    
+</body>
+</html>
